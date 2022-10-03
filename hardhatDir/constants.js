71c13981787033d1afb58525540d0d3213fe578f
RV <valuhar@gmail.com>
2022-10-03 21:48:33 +0530

    first commit

---
+++ hardhatDir/constants.js
@@ -0,0 +1,5 @@
+// Replace the value with your NFT contract address
+const CRYPTODEVS_NFT_CONTRACT_ADDRESS =
+  "0xf8867fd9bF240BdeFd1828fd37ecA0CAb748B7E7";
+
+module.exports = { CRYPTODEVS_NFT_CONTRACT_ADDRESS };
